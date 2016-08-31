aea8f24fdc2ad747c92da4e30f670758c91c217e
郭乐乐 <lel.guo@haihangyun.com>
2016-08-31 17:28:51 +0800

    Initial Commit

---
+++ HNARSADemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNARSADemo
+//
+//  Created by __无邪_ on 16/8/31.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
